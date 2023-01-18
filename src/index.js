25a0ffce67350a76acf2a6d4798061a3becdce8a
pelegsch666 <peleg.mazuz@gmail.com>
2023-01-18 15:57:59 +0200

    last update

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import './assets/styles/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </Provider>
+  
 );
 
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -7,6 +7,7 @@ export function HomePage() {
     <>
       <Sidebar />
       <Postlist />
+      <Postlist />
     </>
   );
 }
